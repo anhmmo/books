16d2c9cd6401e4f47fb6d42e4e27ea34749204a7
anhmmo <aun@metropolia.fi>
2020-07-10 18:29:56 +0300

    create Utils folder and add two files

---
+++ src/Utils/TypeFilter.js
@@ -0,0 +1,16 @@
+const TypeFilter = [
+  {
+    type: "currentlyReading",
+    label: "Currently Reading",
+  },
+  {
+    type: "wantToRead",
+    label: "Want to Read",
+  },
+  {
+    type: "read",
+    label: "Read",
+  },
+];
+
+export default TypeFilter;
